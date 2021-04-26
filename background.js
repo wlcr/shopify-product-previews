8c9315921f6891209a8902ff86152b84a66d970b
Will Christenson <will@wlcr.io>
2021-04-26 09:22:49 -0700

    Cleanup

--- background.js
+++ background.js
@@ -4,110 +4,27 @@ function logMessage(msg) {
   chrome.tabs.executeScript({code:"console.log('"+msg+"')"});
 }
 
-// function getShopUrl(tabId) {
-//   chrome.tabs.executeScript(tabId, {
-//     // code: "console.log(chrome.tabs.executeScript); window.Shopify.shop"
-//     code: "document.head.innerHTML"
-//   },
-//   (result) => {
-//     console.log('result:', result)
-//     shopUrl = result;
-//   }
-//   );
-//   return shopUrl;
-// }
-function getShopUrl(tabId) {
-  chrome.tabs.executeScript(tabId, {
-    file: "content-script.js",
-    allFrames: true
-  });
-}
-// chrome.runtime.onInstalled.addListener(() => {
-//   // chrome.storage.sync.set({ color });
-//   console.log('Testing browser extensions');
-// });
-// console.log('Testing');
-
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-//   // const { tabId, requestId } = details;
-
-  // alert(details);
-  // console.log('Log', details.url);
-  // console.warning('Warn', details);
-  // console.warn('Testing');
-  // chrome.extension.getBackgroundPage().console.log('Test', details);
-
-  // return {cancel: details.url.indexOf("://*myshopify.com/") != -1};
-  // return {cancel: true};
-
-}, { urls: ["<all_urls>"] }, ['blocking']);
-
 chrome.tabs.onUpdated.addListener(function
   (tabId, changeInfo, tab) {
-    // read changeInfo data and do something with it (like read the url)
     if (changeInfo.url) {
-      // do something here
-      // console.log('Change URL', changeInfo.url);
       const newUrl = new URL(changeInfo.url);
       const previewKey = newUrl.searchParams.get('preview_key');
 
       if (previewKey) {
-        // alert(`Preview key is: ${previewKey}`);
-        console.log('GOT URL PREVIEW KEY', previewKey);
-        logMessage(`Preview Key is: ${previewKey}`);
-        logMessage(`Append to URL: /products_preview?preview_key=${previewKey}`);
+        // console.log('GOT URL PREVIEW KEY', previewKey);
+        // logMessage(`Preview Key is: ${previewKey}`);
+        // logMessage(`Append to URL: /products_preview?preview_key=${previewKey}`);
 
         shopifyInfo.previewKey = previewKey;
-
-        // getShopUrl(tabId);
-        // const thisShopUrl = getShopUrl(tabId);
-        // console.log(thisShopUrl);
-        // logMessage(`Shop URL is: ${thisShopUrl}`);
-        // logMessage(`Shop URL is: ${shopUrl}`);
-        
-
-        // if (global.Shopify && global.Shopify.shop) {
-        //   console.log(`${Shopify.shop}/products_preview?preview_key=${previewKey}`);
-        // }
       }
-      ///  https://yu3cwraxluigkl7r-20725133.shopifypreview.com/products_preview?preview_key=7ce20c2d0f66d62b115f3a0e10f8e9ea
     }
   }
 );
 
-// (() => {chrome.webRequest.onBeforeRequest.addListener((details) => {
-// //   // const { tabId, requestId } = details;
-
-//   alert('Testing');
-//   console.log(details);
-
-// }, {urls: ["<all_urls>"]});
-// })();
-
-// var callback = function(details) {
-//   console.log(details);
-// };
-
-// var filter = {...};
-// var opt_extraInfoSpec = [...];
-
-// chrome.webRequest.onBeforeRequest.addListener(
-//   callback, filter, opt_extraInfoSpec);
-
-// chrome.webRequest.onBeforeRequest.addListener(
-//   function(details) {
-//     return {cancel: details.url.indexOf("://www.evil.com/") != -1};
-//   },
-//   {urls: ["<all_urls>"]},
-//   ["blocking"]
-// );
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    // console.log(shopifyInfo);
     if (sender.tab && request.Shopify) {
       shopifyInfo.Shopify = request.Shopify;
-      // console.log(shopifyInfo);
 
       chrome.notifications.create('productPreviewNotification', {
         type: 'basic',
@@ -119,9 +36,5 @@ chrome.runtime.onMessage.addListener(
       
       sendResponse(shopifyInfo);
     }
-    // console.log(sender.tab ?
-    //             "from a content script:" + sender.tab.url :
-    //             "from the extension");
-    // if (request.greeting == "hello") sendResponse({ farewell: "goodbye" });
   }
 );
--- content-script.js
+++ content-script.js
@@ -1,13 +1,3 @@
-// chrome.runtime.sendMessage({ greeting: 'hello' }, function(response) {
-//   // console.log(response.farewell);
-//   // console.log(Shopify);
-//   console.log('Hello from the browser?', document.readyState);
-// });
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   console.log('Document Ready', Shopify)
-// });
-
 document.addEventListener('DOMContentLoaded', (event) => {
   let isShopify = window.Shopify || 'nope';
   var s = document.createElement('script');
@@ -17,26 +7,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }));
   `;
   (document.head||document.documentElement).appendChild(s);
-  // s.onload = function() {
-  //     s.remove();
-  // };
-
-  // console.log('DOMContentLoaded', event, isShopify);
-
-  // const scriptTags = document.head.querySelectorAll('script:not([src])');
-
-  // scriptTags
 }, false);
 
 document.addEventListener('ShopifyProductPreviewsGetShopUrl', function(e) {
-  // e.detail contains the transferred data (can be anything, ranging
-  // from JavaScript objects to strings).
-  // Do something, for example:
-  // alert(e.detail);
-  // console.log('Custom event fired', JSON.parse(e.detail));
-
   chrome.runtime.sendMessage({ Shopify: JSON.parse(e.detail) }, function(shopifyInfo) {
-    // console.log('Response from the Extension?', shopifyInfo);
     const previewUrl = `//${shopifyInfo.Shopify.shop}/products_preview?preview_key=${shopifyInfo.previewKey}`;
 
     console.log(`Preview URL is ${shopifyInfo.Shopify.shop}/products_preview?preview_key=${shopifyInfo.previewKey}`);
@@ -44,14 +18,3 @@ document.addEventListener('ShopifyProductPreviewsGetShopUrl', function(e) {
     window.location.replace(previewUrl);
   });
 });
-
-// const getStoreUrl = () => {
-//   let isShopify = Shopify || false;
-//   console.log('Document Ready. isShopify? ', isShopify)
-// }
-
-// if (document.readyState === 'loading') {  // Loading hasn't finished yet
-//   document.addEventListener('DOMContentLoaded', getStoreUrl);
-// } else {  // `DOMContentLoaded` has already fired
-//   getStoreUrl();
-// }
